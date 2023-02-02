88e13dd530c9e24751d296e08822ba1188d6b21c
Musdaiji69 <114324732+Musdaiji69@users.noreply.github.com>
2023-02-02 11:27:05 +0700

    fix file android manifest, fix halaman login

--- app/src/main/java/com/example/tiketbus/Register.kt
+++ app/src/main/java/com/example/tiketbus/Register.kt
@@ -1,11 +1,17 @@
 package com.example.tiketbus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+    btnRegister.setOnClickListener {
+        val intent = Intent( Intent(this, Register_page::class.java))
+        startActivity(intent)
+    }
     }
 }
--- app/src/main/java/com/example/tiketbus/Register_page.kt
+++ app/src/main/java/com/example/tiketbus/Register_page.kt
@@ -1,5 +1,6 @@
 package com.example.tiketbus
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.tiketbus.databinding.ActivityRegisterPageBinding
+import kotlinx.android.synthetic.main.activity_register_page.*
 
 class Register_page : AppCompatActivity() {
 
@@ -16,20 +18,24 @@ private lateinit var binding: ActivityRegisterPageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
      binding = ActivityRegisterPageBinding.inflate(layoutInflater)
      setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+//        setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_register_page)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+//        binding.fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
